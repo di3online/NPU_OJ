bd6294b4759dff5977a5bd3f8fe36418f7f1c5b8
Laishzh <laishengzhang@gmail.com>
2013-04-26 13:53:50 +0800

    Add TimeUsage, MemorySize, DateTime struct. Modify the watch flows in the launch.cpp.

---
+++ src/dataitem.cpp
@@ -0,0 +1,23 @@
+#include "../include/common.h"
+#include "../include/dataitem.h"
+
+
+void 
+Problem::clear_testcases() {
+    this->prob_testcases.clear();
+}
+
+void 
+Problem::add_testcase(TestCase tc) {
+    this->prob_testcases.push_back(tc);
+}
+
+TestCase *
+Problem::fetch_testcase(size_t loc) {
+    if (loc < this->prob_testcases.size()) {
+        return &this->prob_testcases.at(loc);
+    } else {
+        return NULL;
+    }
+}
+
